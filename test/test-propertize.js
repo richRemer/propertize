8007142b380f931b9b5f04b23af3d7b640771834
Rich Remer <rich.remer@gmail.com>
2014-08-31 11:15:17 -0700

    remove getter/setter variants, complete flag variants, refactor tests

--- test/test-propertize.js
+++ test/test-propertize.js
@@ -2,44 +2,116 @@ var prop = require("..")
     expect = require("expect.js");
 
 describe("propertize", function() {
-    describe(".internal", function()  {
-        it("should add internal object property", function() {
+    function testAdd(fn) {
+        return function() {
+            var obj = {};
+            prop[fn](obj, "a", 1);
+            expect(obj.a).to.be(1);        
+        };
+    }
+    
+    function testUpdate(fn) {
+        return function() {
+            var obj = {a: 1};
+            prop[fn](obj, "a", 2);
+            expect(obj.a).to.be(2);
+        };
+    }
+    
+    function testNoConfig(fn) {
+        return function() {
+            var obj = {};
+            prop[fn](obj, "a", 1);
+            expect(delete obj.a).to.be(false);
+        };
+    }
+    
+    function testNoEnumerate(fn) {
+        return function() {
             var obj = {},
-                hidden = true;
+                enumerated = false,
+                name;
+                
+            prop[fn](obj, "a", 1);
+            for (name in obj) if (name === "a") enumerated = true;
+            expect(enumerated).to.be(false);
+        };
+    }
+    
+    function testNoWrite(fn) {
+        return function() {
+            var obj = {};
 
-            prop.internal(obj, "a", 1);
-            expect(obj.a).to.be(1);
-            
+            prop[fn](obj, "a", 1);
             obj.a = 2;
-            expect(obj.a).to.be(1);
-            
-            for (var name in obj) if (name === "a") hidden = false;
-            expect(hidden).to.be.ok();
-        });
+            expect(obj.a).to.be(1);        
+        };
+    }
+
+    describe(".field", function() {
+        it("should add a new property", testAdd("field"));
+        
+        it("should update an existing property", testUpdate("field"));
+        
+        it("should prevent further configuration", testNoConfig("field"));
+    });
+    
+    describe(".hidden", function() {
+        it("should add a new property", testAdd("hidden"));
+        
+        it("should update an existing property", testUpdate("hidden"));
+        
+        it("should prevent property enumeration", testNoEnumerate("hidden"));
     });
 
     describe(".readonly", function() {
-        it("should add read-only object property", function() {
-            var obj = {};
+        it("should add a new property", testAdd("readonly"));
+        
+        it("should update an existing property", testUpdate("readonly"));
+        
+        it("should prevent writing to the property", testNoWrite("readonly"));
+    });
 
-            prop.readonly(obj, "a", 1);
-            expect(obj.a).to.be(1);
+    describe(".internal", function()  {
+        it("should add a new property", testAdd("internal"));
+        
+        it("should update an existing property", testUpdate("internal"));
+        
+        it("should prevent property enumeration", testNoEnumerate("internal"));
+        
+        it("should prevent writing to the property", testNoWrite("internal"));
+    });
 
-            obj.a = 2;
-            expect(obj.a).to.be(1);
-        });
+    describe(".attribute", function() {
+        it("should add a new property", testAdd("attribute"));
+        
+        it("should update an existing property", testUpdate("attribute"));
+        
+        it("should prevent further configuration", testNoConfig("attribute"));
+        
+        it("should prevent writing to the property", testNoWrite("attribute"));
     });
     
-    describe(".calculated", function() {
-        it("should add calculated object property", function() {
-            var obj = {a: 1};
-            
-            prop.calculated(obj, "b", function() {return this.a+1;});
-            expect(obj.b).to.be(2);
-            
-            obj.a = 3;
-            expect(obj.b).to.be(4);
-        });
-    })
+    describe(".setting", function() {
+        it("should add a new property", testAdd("setting"));
+        
+        it("should update an existing property", testUpdate("setting"));
+        
+        it("should prevent further configuration", testNoConfig("setting"));
+        
+        it("should prevent property enumeration", testNoEnumerate("setting"));
+    });
+    
+    describe(".locked", function() {
+        it("should add a new property", testAdd("locked"));
+        
+        it("should update an existing property", testUpdate("locked"));
+        
+        it("should prevent further configuration", testNoConfig("locked"));
+        
+        it("should prevent property enumeration", testNoEnumerate("locked"));
+
+        it("should prevent writing to the property", testNoWrite("locked"));
+    });
 });
 
